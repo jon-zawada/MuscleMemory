c2de6db7d3d6f972e2890215ed6e450c3218cfff
jon-zawada <jonathan_zawada@yahoo.com>
2026-03-29 13:07:48 -0700

    chore: fix prettier

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import type { Request, Response } from 'express';
+import express from "express";
+import type { Request, Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World from Express!");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
-})
+});
