744422acf68912864fb2dafb970e79521098c904
jon-zawada <jonathan_zawada@yahoo.com>
2026-03-30 01:44:29 -0700

    chore: code review, formatting

--- server/src/db/migrations/004_create_exercise_logs.sql
+++ server/src/db/migrations/004_create_exercise_logs.sql
@@ -1,5 +1,5 @@
 CREATE TABLE exercise_logs (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
   workout_id UUID NOT NULL REFERENCES workouts (id) ON DELETE CASCADE,
   exercise_id UUID NOT NULL REFERENCES exercises (id) ON DELETE CASCADE,
   order_index SMALLINT,
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,15 @@ import dotenv from "dotenv";
 import path from "path";
 import type { Request, Response } from "express";
 import { logger } from "./utils/logger";
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World from Express!");
 });
 
 app.listen(PORT, () => {
-  logger.log(`Example app listening on port ${PORT}`);
+  logger.log(`[SERVER] listening on port ${PORT}`);
 });
