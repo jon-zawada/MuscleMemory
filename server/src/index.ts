6fcfc30046a00e4864ffff696809a7a6f07d4ede
jon-zawada <jonathan_zawada@yahoo.com>
2026-04-02 23:36:07 -0700

    nit: change server port, working on client now it will conflict

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import authRouter from "./routes/authRoutes";
 import { logger } from "./utils/logger";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
