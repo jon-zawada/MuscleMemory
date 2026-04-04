f7611fdd7977716d4a675668ef197c81d83f4907
jon-zawada <jonathan_zawada@yahoo.com>
2026-04-03 19:20:33 -0700

    chore: add react-router and login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,14 @@
-const App = () => <div>HELLO WORLD!</div>;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<div>dashboard</div>} />
+      <Route path="/coach/dashboard" element={<div>Coach dashboard</div>} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
